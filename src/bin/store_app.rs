619d7581178c036cdb99e3d8cdf21fe781dcbbe3
WilliamZhu <allwefantasy@gmail.com>
2019-09-12 14:46:23 +0800

    update

--- src/bin/store_app.rs
+++ src/bin/store_app.rs
@@ -6,9 +6,21 @@ use rocket::routes;
 use mlsql_app_store::controller;
 use mlsql_app_store::base::db::MyPool;
 use mlsql_app_store::base::config::Config;
+use rocket::config::Environment;
+use rocket_contrib::serve::StaticFiles;
 
 fn main() {
     let config = Config::new();
     let db = MyPool::new(&config);
-    rocket::ignite().manage(db).mount("/", routes![controller::store_controller::showTable]).launch();
+
+    let server_config = rocket::Config::build(Environment::Staging)
+        .address("localhost")
+        .port(8001)
+        .finalize().unwrap();
+    //.mount("/public", StaticFiles::from("/static"))
+    rocket::custom(server_config).manage(db).mount("/", routes![
+    controller::store_controller::index,
+    controller::store_controller::download_plugin,
+    controller::store_controller::plugin_store_list
+    ]).launch();
 }
--- src/controller/store_controller.rs
+++ src/controller/store_controller.rs
@@ -1,21 +1,33 @@
-use rocket::response::content;
-use rocket::{get, State};
+use rocket::response::{content, Stream, NamedFile, Responder};
+use rocket::{post, get, State, Request, Response};
+use rocket::request::{FromForm, Form};
+use rocket::http::Status;
 use serde::{Deserialize, Serialize};
 use crate::base::db::MyPool;
 use mysql as my;
 use std::sync::mpsc::Sender;
+use std::io;
+use std::fs::File;
+use serde_json;
+use std::path::PathBuf;
+
 
 #[get("/jack")]
-pub fn showTable(db: State<MyPool>) -> content::Json<String> {
+pub fn index() -> &'static str {
+    "Hello, world!"
+}
+
+#[get("/repo/plugins?<name>")]
+pub fn plugin_store_list(db: State<MyPool>, name: String) -> content::Json<String> {
     let db_ref = db.value();
-    let query = async move |tx_c: Sender<Result<Vec<HelloRes>, my::error::Error>>| {
+    let query = async move |tx_c: Sender<Result<Vec<PluginsStore>, my::error::Error>>| {
         let query_res = db_ref.prep_exec(
-            "select name,url from backend", ()).unwrap();
+            "select id,name,location,official,enable from plugins_store where name=?", (name, )).unwrap();
 
         let res = query_res.map(|x| x.unwrap()).map(|row| {
-            let (name, url) = my::from_row::<(String, String)>(row);
-            HelloRes { name, url }
-        }).collect::<Vec<HelloRes>>();
+            let (id, name, location, official, enable) = my::from_row::<(i32, String, String, i32, i32)>(row);
+            PluginsStore { id, name, location, official, enable }
+        }).collect::<Vec<PluginsStore>>();
         tx_c.send(Ok(res)).unwrap();
         ()
     };
@@ -26,8 +38,54 @@ pub fn showTable(db: State<MyPool>) -> content::Json<String> {
     }
 }
 
+pub enum PathResp { File(PathBuf), ErrorMessage(String) }
+
+impl Responder<'static> for PathResp {
+    fn respond_to(self, req: &Request) -> Result<Response<'static>, Status> {
+        match self {
+            PathResp::File(path) => NamedFile::open(path).ok().respond_to(req),
+            PathResp::ErrorMessage(msg) => {
+                content::Plain(msg).respond_to(req)
+            }
+        }
+    }
+}
+
+
+#[post("/repo/plugins/download", data = "<req>")]
+pub fn download_plugin(db: State<MyPool>, req: Form<DownloadReq>) -> PathResp {
+    let db_ref = db.value();
+    let name = &req.name.clone();
+    let query = async move |tx_c: Sender<Result<Vec<PluginsStore>, my::error::Error>>| {
+        let query_res = db_ref.prep_exec(
+            "select id,name,location,official,enable from plugins_store where name=?", (&req.name, )).unwrap();
+
+        let res = query_res.map(|x| x.unwrap()).map(|row| {
+            let (id, name, location, official, enable) = my::from_row::<(i32, String, String, i32, i32)>(row);
+            PluginsStore { id, name, location, official, enable }
+        }).collect::<Vec<PluginsStore>>();
+        tx_c.send(Ok(res)).unwrap();
+        ()
+    };
+    let res: Vec<PluginsStore> = db.future_exec(query).unwrap();
+    //let file = File::open(&res[0].location);
+    //file.map(|x| Stream::from(x))
+    let zero: usize = 0;
+    if &res.len() > &zero { PathResp::File(PathBuf::from(&res[0].location)) } else {
+        PathResp::ErrorMessage(String::from(format!("{} is not exists", name)))
+    }
+}
+
 #[derive(Debug, Serialize, Deserialize)]
-struct HelloRes {
+struct PluginsStore {
+    id: i32,
     name: String,
-    url: String,
+    location: String,
+    official: i32,
+    enable: i32,
+}
+
+#[derive(FromForm)]
+pub struct DownloadReq {
+    name: String,
 }
